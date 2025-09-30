fbc8e5ba810960fe332a429c8338f1290e799558
Adrian476 <arodriguez476@ikasle.ehu.eus>
2025-09-30 14:26:03 +0200

    class MockTest getRideStopsByCod

---
+++ src/test/java/GetRideStopsMockWhiteTest.java
@@ -0,0 +1,92 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import dataAccess.DataAccess;
+import domain.User;
+import domain.Driver;
+import domain.Ride;
+import exceptions.RideAlreadyExistException;
+import exceptions.RideMustBeLaterThanTodayException;
+
+public class GetRideStopsMockWhiteTest {
+	
+	static DataAccess sut;
+	
+	protected MockedStatic <Persistence> persistenceMock;
+
+	@Mock
+	protected  EntityManagerFactory entityManagerFactory;
+	@Mock
+	protected  EntityManager db;
+	@Mock
+    protected  EntityTransaction  et;
+	
+
+	@Before
+    public  void init() {
+        MockitoAnnotations.openMocks(this);
+        persistenceMock = Mockito.mockStatic(Persistence.class);
+		persistenceMock.when(() -> Persistence.createEntityManagerFactory(Mockito.any()))
+        .thenReturn(entityManagerFactory);
+        
+        Mockito.doReturn(db).when(entityManagerFactory).createEntityManager();
+		Mockito.doReturn(et).when(db).getTransaction();
+	    sut=new DataAccess(db);
+
+
+		
+    }
+	@After
+    public  void tearDown() {
+		persistenceMock.close();	
+    }
+	
+	
+	Driver driver;
+
+	@Test
+	 
+	public void test1() {
+		User u1 = new User("u1@gmail.com", "u1", "u1", "T");
+		u1.getTraveler().setEmail("u1@gmail.com");
+		
+		try {
+			//configure the state through mocks 
+	        Mockito.when(db.find(User.class, u1.getEmail())).thenReturn(u1);	
+			
+			
+			//invoke System Under Test (sut)  
+			sut.open();
+			sut.registerUser(u1);
+			sut.close();
+			assertTrue(true);
+			
+			
+		} catch (Exception e) {
+			 //verify the results
+				sut.close();
+				fail();
+		}
+	} 
+}
