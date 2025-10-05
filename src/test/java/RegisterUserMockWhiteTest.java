dc2817baea44cdffaf1cf484f2fb9513c7fc6532
Shirley <sosorio002@ikasle.ehu.eus>
2025-10-05 12:39:01 +0200

    RegisterUserMock caja blanca test termiando

--- src/test/java/RegisterUserMockWhiteTest.java
+++ src/test/java/RegisterUserMockWhiteTest.java
@@ -67,50 +67,167 @@ public class RegisterUserMockWhiteTest {
 	Driver driver;
 
 	@Test
-	//sut.registerUser:  The User1(“u1@gmail.com”, “u1”, “u1”, “T”) IS on the DB. 
+	//sut.registerUser:  The UserT(“uT@gmail.com”, “uT”, “uT”, “T”) IS on the DB. 
 	//OK: no hace persist
 	//FAIL: hace persist
 	public void test1() {
 		
-		User u1 = new User("u1@gmail.com", "u1", "u1", "T");
-		u1.getTraveler().setEmail("u1@gmail.com");
+		User uT = new User("uT@gmail.com", "uT", "uT", "T");
+		uT.getTraveler().setEmail("uT@gmail.com");
 		//configure the state through mocks 
-		Mockito.when(db.find(User.class, u1.getEmail())).thenReturn(u1);	
+		Mockito.when(db.find(User.class, uT.getEmail())).thenReturn(uT);	
 
 
 		//invoke System Under Test (sut)  
 		sut.open();
-		User u = sut.registerUser(u1);
+		User u = sut.registerUser(uT);
 		sut.close();
-		Mockito.verify(db, Mockito.times(0)).persist(u);
+		Mockito.verify(db, Mockito.times(0)).persist(uT);
 
 
 
 	}
 	@Test
-	//sut.registerUser:  The User1(“u1@gmail.com”, “u1”, “u1”, “T”) IS NOT on the DB. 
-	// OK: persist con parametros “u1@gmail.com”, “u1”, “u1”, “T”
+	//sut.registerUser: TRAVELER The UserT(“uT@gmail.com”, “uT”, “uT”, “T”) IS NOT on the DB. 
+	// OK: persist con parametros “uT@gmail.com”, “uT”, “uT”, “T”
 	// FAIL: no hace persist o hace persist con valores nulos?
 	public void test2() {
 		
-		User u1 = new User("u1@gmail.com", "u1", "u1", "T");
-		u1.getTraveler().setEmail("u1@gmail.com");
+		User uT = new User("uT@gmail.com", "uT", "uT", "T");
+		uT.getTraveler().setEmail("uT@gmail.com");
 		//configure the state through mocks 
-		Mockito.when(db.find(User.class, u1.getEmail())).thenReturn(null);	
+		Mockito.when(db.find(User.class, uT.getEmail())).thenReturn(null);	
+		
 		
 		//invoke System Under Test (sut)  
 		sut.open();
-		User u = sut.registerUser(u1);
+		sut.registerUser(uT);
 		sut.close();
-		//define	Argument	captors
-		ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
-		//verify	call	numbers	and	capture	parameters
+		
+		
+		//define Argument captors
+		ArgumentCaptor<User> userCaptor =
+				ArgumentCaptor.forClass(User.class);
+		//verify call numbers and capture parameters
 		Mockito.verify(db,Mockito.times(1)).persist(userCaptor.capture());
-		//verify	parameter	values	as	usual	using	JUnit	asserts
+		//verify parameter values as usual using JUnit asserts
+		assertEquals(uT.getEmail(),(userCaptor.getValue().getEmail()));
+		assertEquals(uT.getPassword(),(userCaptor.getValue().getPassword()));
+		assertEquals(null,(userCaptor.getValue().getDriver()));
+		assertEquals(uT.getTraveler(),(userCaptor.getValue().getTraveler()));
 	
-
-
 	}
+	
+	@Test
+	//sut.registerUser: TRAVELER The The UserT(“uT@gmail.com”, “uT”, “uT”, “T”) IS NOT on the DB. 
+	// OK: persist con parametros “uT@gmail.com”, “uT”, “uT”, “T”
+	// FAIL: no hace persist o hace persist con valores nulos?
+	public void test2PERSIST() {
+		
+		User uT = new User("uT@gmail.com", "uT", "uT", "T");
+		uT.getTraveler().setEmail("uT@gmail.com");
+		//configure the state through mocks 
+		Mockito.when(db.find(User.class, uT.getEmail())).thenReturn(null);	
+		
+		
+		//invoke System Under Test (sut)  
+		sut.open();
+		sut.registerUser(uT);
+		sut.close();
+		
+		//System.out.println(u);
+		Mockito.verify(db, Mockito.times(1)).persist(uT);
+		
+	}
+	
+	@Test
+	//sut.registerUser: DRIVER The UserD(“uD@gmail.com”, “uD”, “uD”, “D”) IS NOT on the DB. 
+	// OK: persist con parametros “uD@gmail.com”, “uD”, “uD”, “D”
+	// FAIL: no hace persist o hace persist con valores nulos?
+	public void test3() {
+		
+		User uD = new User("uD@gmail.com", "uD", "uD", "D");
+		uD.getDriver().setEmail("uD@gmail.com");
+	
+		//configure the state through mocks 
+		Mockito.when(db.find(User.class, uD.getEmail())).thenReturn(null);	
+		
+		
+		//invoke System Under Test (sut)  
+		sut.open();
+		sut.registerUser(uD);
+		sut.close();
+		
+		//define Argument captors
+		ArgumentCaptor<User> userCaptor =
+				ArgumentCaptor.forClass(User.class);
+		//verify call numbers and capture parameters
+		Mockito.verify(db,Mockito.times(1)).persist(userCaptor.capture());
+		//verify parameter values as usual using JUnit asserts
+		assertEquals(uD.getEmail(),(userCaptor.getValue().getEmail()));
+		assertEquals(uD.getPassword(),(userCaptor.getValue().getPassword()));
+		assertEquals(uD.getDriver(),(userCaptor.getValue().getDriver()));
+		assertEquals(null,(userCaptor.getValue().getTraveler()));
+
+		
+	}
+	@Test
+	//sut.registerUser: The User(“u@gmail.com”, “u”, “u”, "" ) IS NOT on the DB. 
+	// OK: persist con parametros “u@gmail.com”, “u”, “u”, “”
+	// FAIL: no hace persist o hace persist con valores nulos?
+	public void test4() {
+		
+		User u = new User("u@gmail.com", "u", "u", "");
+		
+	
+		//configure the state through mocks 
+		Mockito.when(db.find(User.class, u.getEmail())).thenReturn(null);	
+		
+		
+		//invoke System Under Test (sut)  
+		sut.open();
+		sut.registerUser(u);
+		sut.close();
+		
+		//define Argument captors
+		ArgumentCaptor<User> userCaptor =
+				ArgumentCaptor.forClass(User.class);
+		//verify call numbers and capture parameters
+		Mockito.verify(db,Mockito.times(1)).persist(userCaptor.capture());
+		//verify parameter values as usual using JUnit asserts
+		assertEquals(u.getEmail(),(userCaptor.getValue().getEmail()));
+		assertEquals(u.getPassword(),(userCaptor.getValue().getPassword()));
+		assertEquals(null,(userCaptor.getValue().getDriver()));
+		assertEquals(null,(userCaptor.getValue().getTraveler()));
+
+		
+	}
+		
+	@Test
+	//sut.registerUser: The User(“u@gmail.com”, “u”, “u”, "" ) IS on the DB. 
+	// OK: no hace persist 
+	// FAIL: hace persist
+	public void test5() {
+		
+		User u = new User("u@gmail.com", "u", "u", "");
+		
+	
+		//configure the state through mocks 
+		Mockito.when(db.find(User.class, u.getEmail())).thenReturn(u);	
+		
+		
+		//invoke System Under Test (sut)  
+		sut.open();
+		sut.registerUser(u);
+		sut.close();
+		
+		Mockito.verify(db, Mockito.times(0)).persist(u);
+
+		
+	}
+
+
+	
 	
 	
 	
