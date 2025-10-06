29432cef57ead6faadc1e6ce66dde23aac47da30
Shirley <sosorio002@ikasle.ehu.eus>
2025-10-06 11:19:19 +0200

    Register user bd white test 2

--- src/test/java/RegisterUserBDWhiteTest.java
+++ src/test/java/RegisterUserBDWhiteTest.java
@@ -72,6 +72,117 @@ public class RegisterUserBDWhiteTest {
 		}
 		
 	} 
+	
+	@Test
+	//sut.registerUser: TRAVELER The UserT(“uT@gmail.com”, “uT”, “uT”, “T”) IS NOT on the DB. 
+	// OK: persist con parametros “uT@gmail.com”, “uT”, “uT”, “T”
+	// FAIL: no hace persist o hace persist con valores nulos?
+	public void test2() {
+		
+		boolean existUser = false;
+		User uT = new User("uT@gmail.com", "uT", "uT", "T");
+
+		
+		try {
+			//configure the state of the system (create object in the database)
+			testDA.open();
+			existUser = testDA.existUser(uT);
+			if (existUser) testDA.removeUser(uT);
+			//no debería existir, tiene que ser null
+			User userAntesNULL = testDA.findUser(uT);
+			//DEBERÍA SER CERO
+			int countBefore0 = testDA.countUsersByEmail(uT.getEmail());
+			testDA.close();		
+			
+			assertNull(userAntesNULL);
+			assertEquals(0, countBefore0);
+			
+			
+			//invoke System Under Test (sut)  
+			sut.open();
+			User userDevuelto = sut.registerUser(uT);
+			sut.close();
+			
+			
+
+			testDA.open();
+			//DEBERÍA SER 1
+	        int countAfter1 = testDA.countUsersByEmail(uT.getEmail());
+	        User userPersistido = testDA.findUser(uT);
+	        testDA.close();
+	        
+	        
+	        assertNotNull(userDevuelto);
+	        assertNotNull(userPersistido);
+	        assertEquals(uT.getEmail(), userDevuelto.getEmail());
+	        
+	        assertEquals(countBefore0 + 1, countAfter1);
+
+			
+		} finally {
+			//Remove the created objects in the database (cascade removing)   
+			testDA.open();
+			if (!existUser) testDA.removeUser(uT);
+			testDA.close();
+		}
+		
+	}
+	
+	@Test
+	//sut.registerUser: DRIVER The UserD(“uD@gmail.com”, “uD”, “uD”, “D”) IS NOT on the DB. 
+	// OK: persist con parametros “uD@gmail.com”, “uD”, “uD”, “D”
+	// FAIL: no hace persist o devuelve valores nulos?
+	public void test3() {
+		
+		boolean existUser = false;
+		User uD = new User("uD@gmail.com", "uD", "uD", "D");
+
+		
+		try {
+			//configure the state of the system (create object in the database)
+			testDA.open();
+			existUser = testDA.existUser(uD);
+			if (existUser) testDA.removeUser(uD);
+			//no debería existir, tiene que ser null
+			User userAntesNULL = testDA.findUser(uD);
+			//DEBERÍA SER CERO
+			int countBefore0 = testDA.countUsersByEmail(uD.getEmail());
+			testDA.close();		
+			
+			assertNull(userAntesNULL);
+			assertEquals(0, countBefore0);
+			
+			
+			//invoke System Under Test (sut)  
+			sut.open();
+			User userDevuelto = sut.registerUser(uD);
+			sut.close();
+			
+			
+
+			testDA.open();
+			//DEBERÍA SER 1
+	        int countAfter1 = testDA.countUsersByEmail(uD.getEmail());
+	        User userPersistido = testDA.findUser(uD);
+	        testDA.close();
+	        
+	        
+	        assertNotNull(userDevuelto);
+	        assertNotNull(userPersistido);
+	        assertEquals(uD.getEmail(), userDevuelto.getEmail());
+	        
+	        assertEquals(countBefore0 + 1, countAfter1);
+
+			
+		} finally {
+			//Remove the created objects in the database (cascade removing)   
+			testDA.open();
+			if (!existUser) testDA.removeUser(uD);
+			testDA.close();
+		}
+		
+	}
+	
 
 
 
