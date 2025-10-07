34cff0fbc7dbc253065906517d80e2b4ec0be47e
aaldanas <140650874+aaldanas@users.noreply.github.com>
2025-10-07 22:47:59 +0200

    Caja negra y Caja blanca LogIn

--- src/test/java/LogInUserMockWhiteTest.java
+++ src/test/java/LogInUserMockWhiteTest.java
@@ -1,15 +1,8 @@
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,9 +10,7 @@ import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -27,7 +18,6 @@ import org.mockito.MockitoAnnotations;
 
 import dataAccess.DataAccess;
 import domain.User;
-import domain.Driver;
 
 public class LogInUserMockWhiteTest {
 	//system under test
@@ -81,31 +71,12 @@ public class LogInUserMockWhiteTest {
 	//	Mockito.verify(db, Mockito.times(0)).persist(u);
 	}
 	
-	@Test
-	//sut.login:  The UserT(null, “uT”, “pas”, “T”) must return null. 
-	//OK: devuelve null
-	//FAIL: devuelve un usuario
-	public void test2() {
-		email =null;
-		password="pas";
-		User ut = new User(email, "uT", password, "T");
-		//configure the state through mocks 
-		Mockito.when(db.find(User.class, ut.getEmail())).thenReturn(null);	
-		//invoke System Under Test (sut)  
-		sut.open();
-		User u = sut.loginUser(email, password);
-		sut.close();
-
-		assertNull(u);
-	//	Mockito.verify(db, Mockito.times(0)).persist(u);
-	}
-	
 	//sut.login:  The User(“email”, “u”, password, "T" ) IS in the DB
 	//			  and the password is incorrect
 	//OK: returns the user with email, u and empty password
 	//FAIL: returns null or the user with the incorrect password
 	@Test
-	public void test3() {
+	public void test2() {
 		String email ="u1@gmail.com";
 		String password="incorrecta";
 		User ut = new User(email, "uT", "CORRECTA", "T");
@@ -120,15 +91,18 @@ public class LogInUserMockWhiteTest {
 		assertNull(u.getPassword());
 	}
 	@Test
-	//sut.login:  The User(“u1@gmail.com", “uT”, “correcta”, "T") IS on the DB.
-	public void test4() {
+	/*
+	 * sut.login:  The User(“u1@gmail.com", “uT”, “correcta”, "T") IS on the DB.
+	 * OK : Returns the traveller object and prints the info
+	 */
+	public void test3() {
 		// Redirige System.out a un stream temporal
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		PrintStream originalOut = System.out;
 		System.setOut(new PrintStream(outContent));
 		    
 		String email1 ="traveller@gmail.com";
-		String password1="traveller";
+		String password1="traveler";
 		User uT = new User(email1, "uT", password1,"T");
 		//configure the state through mocks 
 		Mockito.when(db.find(User.class, uT.getEmail())).thenReturn(uT);	
@@ -142,14 +116,15 @@ public class LogInUserMockWhiteTest {
 		System.setOut(originalOut);
 		String salida = outContent.toString();
 		//texto esperado
-	    assertTrue(salida.contains((CharSequence) u.getTraveler().getAcceptedRides()));
-
+	    assertTrue(salida.contains(u.getTraveler().getAcceptedRides().toString()));
+	    assertNotNull(u.getTraveler());
 
 
 	}
 	@Test
-	//sut.login:  The User(“
-		public void test5() {
+	//sut.login:  The User(“u1@gmail.com", “uT”, “correcta”, "T") IS on the DB.
+	 // OK : Returns the traveller object and prints the info
+		public void test4() {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		PrintStream originalOut = System.out;
 		System.setOut(new PrintStream(outContent));
@@ -169,8 +144,10 @@ public class LogInUserMockWhiteTest {
 		System.setOut(originalOut);
 		String salida = outContent.toString();
 		//texto esperado
-	    assertTrue(salida.contains((CharSequence) u.getTraveler().getAcceptedRides()));
-
+	    assertTrue(salida.contains(u.getDriver().getCreatedRides().toString()));
+	    assertTrue(salida.contains(u.getDriver().getAcceptedRides().toString()));
+	    assertTrue(salida.contains(u.getDriver().getBookedRides().toString()));
+	    assertNotNull(u.getDriver());
 	}
 
 	
